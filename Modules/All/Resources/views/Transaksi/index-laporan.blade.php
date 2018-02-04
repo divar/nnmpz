569f45e1569f5912733315de84a3118002928732
divar <divarart@gmail.com>
2018-02-05 05:37:02 +0700

    alksdoa

--- Modules/All/Resources/views/Transaksi/index-laporan.blade.php
+++ Modules/All/Resources/views/Transaksi/index-laporan.blade.php
@@ -127,7 +127,7 @@ function refresh(){
             },
             { extend: 'excel', className: 'btn btn-sm btn-info',text: '<i class="fa fa-file-excel-o"> export excel</i>',
                 exportOptions:{
-                   columns:[0,1,2,3,4,5,6,7,8,9,10]
+                   columns:[0,1,2,3,4,5,6,7,8,9,10,@for ($i = 0; $i < $satuan ; $i++) {{ $i+10 }}{{ ',' }} @endfor]
                 }
             }
 
