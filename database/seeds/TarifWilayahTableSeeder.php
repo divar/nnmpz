e9908fe548eff8ff7c31b32207c39f2cedc12300
mahendradipra <mahendradipra@gmail.com>
2018-08-25 09:54:25 +0700

    message

--- database/seeds/TarifWilayahTableSeeder.php
+++ database/seeds/TarifWilayahTableSeeder.php
@@ -14,12 +14,12 @@ class TarifWilayahTableSeeder extends Seeder
         DB::table('tarif_wilayahs')->delete();
 
         $data = [
-            ['id'=>1,'nama'=>'P1','harga'=>'10000','keterangan'=>'P1','created_at'=>date('Y-m-d H:i:s')],
-            ['id'=>2,'nama'=>'P2','harga'=>'20000','keterangan'=>'P2','created_at'=>date('Y-m-d H:i:s')],
-            ['id'=>3,'nama'=>'P3','harga'=>'30000','keterangan'=>'P3','created_at'=>date('Y-m-d H:i:s')],
-            ['id'=>4,'nama'=>'P4','harga'=>'15000','keterangan'=>'P4','created_at'=>date('Y-m-d H:i:s')],
-            ['id'=>5,'nama'=>'P5','harga'=>'20000','keterangan'=>'P5','created_at'=>date('Y-m-d H:i:s')],
-            ['id'=>6,'nama'=>'P6','harga'=>'25000','keterangan'=>'P6','created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>1,'nama'=>'Area 1','harga'=>'10000','keterangan'=>'P1','created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>2,'nama'=>'Area 2','harga'=>'20000','keterangan'=>'P2','created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>3,'nama'=>'Area 3','harga'=>'30000','keterangan'=>'P3','created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>4,'nama'=>'Area 4','harga'=>'15000','keterangan'=>'P4','created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>5,'nama'=>'Area 5','harga'=>'20000','keterangan'=>'P5','created_at'=>date('Y-m-d H:i:s')],
+            ['id'=>6,'nama'=>'Area 6','harga'=>'25000','keterangan'=>'P6','created_at'=>date('Y-m-d H:i:s')],
         ];
 
         \DB::table('tarif_wilayahs')->insert($data);
