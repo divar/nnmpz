6d74059d9d199df29d166173d084e2a7a4dfc4b8
mahendradipra <mahendradipra@gmail.com>
2018-08-25 12:20:10 +0700

    message

--- database/seeds/JalanTableSeeder.php
+++ database/seeds/JalanTableSeeder.php
@@ -27,7 +27,7 @@ class JalanTableSeeder extends Seeder
         DB::table('jenis')->delete();
 
         $data = [
-            ['id'=>1,'id_tarif_wilayah'=>1,'jenis'=>'35 Homestay','Prime Plaza Hotel Jogjalarta','Embe 6','Kelurahan','created_at'=>date('Y-1-1 H:i:s')],
+            ['id'=>1,'id_tarif_wilayah'=>1,'jenis'=>'35 Homestay','created_at'=>date('Y-1-1 H:i:s')],
             ['id'=>2,'id_tarif_wilayah'=>2,'jenis'=>'Kos Kosan Gayam','created_at'=>date('Y-1-1 H:i:s')],
             ['id'=>3,'id_tarif_wilayah'=>3,'jenis'=>'Resort','created_at'=>date('Y-1-1 H:i:s')],
             ['id'=>4,'id_tarif_wilayah'=>4,'jenis'=>'Kantor','created_at'=>date('Y-1-1 H:i:s')],
